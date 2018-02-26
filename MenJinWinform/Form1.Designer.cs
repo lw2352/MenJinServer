b2aabdb55dcc7adc7ba20912948e8753ac9f25c6
wei li <liweiliwei0103@outlook.com>
2018-02-26 13:00:28 +0800

    服务部分准备用topself来做

--- MenJinWinform/Form1.Designer.cs
+++ MenJinWinform/Form1.Designer.cs
@@ -29,25 +29,79 @@
         private void InitializeComponent()
         {
             this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.installService = new System.Windows.Forms.Button();
+            this.checkService = new System.Windows.Forms.Button();
             this.tabPage1 = new System.Windows.Forms.TabPage();
-            this.tabPage2 = new System.Windows.Forms.TabPage();
-            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
-            this.checkedListBox2 = new System.Windows.Forms.CheckedListBox();
-            this.searchDevice = new System.Windows.Forms.Button();
             this.selectDevice = new System.Windows.Forms.Button();
+            this.searchDevice = new System.Windows.Forms.Button();
+            this.checkedListBox2 = new System.Windows.Forms.CheckedListBox();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.tabControl1.SuspendLayout();
+            this.tabPage3.SuspendLayout();
+            this.groupBox1.SuspendLayout();
             this.tabPage1.SuspendLayout();
             this.SuspendLayout();
             // 
             // tabControl1
             // 
+            this.tabControl1.Controls.Add(this.tabPage3);
             this.tabControl1.Controls.Add(this.tabPage1);
             this.tabControl1.Controls.Add(this.tabPage2);
-            this.tabControl1.Location = new System.Drawing.Point(13, 13);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(645, 511);
+            this.tabControl1.Size = new System.Drawing.Size(670, 536);
+            this.tabControl1.SizeMode = System.Windows.Forms.TabSizeMode.Fixed;
             this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.groupBox1);
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Size = new System.Drawing.Size(662, 510);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "UDP服务";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.installService);
+            this.groupBox1.Controls.Add(this.checkService);
+            this.groupBox1.Location = new System.Drawing.Point(3, 3);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(663, 265);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "配置服务";
+            // 
+            // installService
+            // 
+            this.installService.Location = new System.Drawing.Point(5, 80);
+            this.installService.Name = "installService";
+            this.installService.Size = new System.Drawing.Size(123, 49);
+            this.installService.TabIndex = 0;
+            this.installService.Text = "installService";
+            this.installService.UseVisualStyleBackColor = true;
+            // 
+            // checkService
+            // 
+            this.checkService.Location = new System.Drawing.Point(139, 80);
+            this.checkService.Name = "checkService";
+            this.checkService.Size = new System.Drawing.Size(123, 49);
+            this.checkService.TabIndex = 1;
+            this.checkService.Text = "checkService";
+            this.checkService.UseVisualStyleBackColor = true;
             // 
             // tabPage1
             // 
@@ -58,54 +112,81 @@
             this.tabPage1.Location = new System.Drawing.Point(4, 22);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(637, 485);
+            this.tabPage1.Size = new System.Drawing.Size(662, 510);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "搜索设备";
             this.tabPage1.UseVisualStyleBackColor = true;
             // 
-            // tabPage2
+            // selectDevice
             // 
-            this.tabPage2.Location = new System.Drawing.Point(4, 22);
-            this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(637, 485);
-            this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "发送命令";
-            this.tabPage2.UseVisualStyleBackColor = true;
-            // 
-            // checkedListBox1
-            // 
-            this.checkedListBox1.FormattingEnabled = true;
-            this.checkedListBox1.Location = new System.Drawing.Point(4, 4);
-            this.checkedListBox1.Name = "checkedListBox1";
-            this.checkedListBox1.Size = new System.Drawing.Size(238, 340);
-            this.checkedListBox1.TabIndex = 0;
-            // 
-            // checkedListBox2
-            // 
-            this.checkedListBox2.FormattingEnabled = true;
-            this.checkedListBox2.Location = new System.Drawing.Point(392, 3);
-            this.checkedListBox2.Name = "checkedListBox2";
-            this.checkedListBox2.Size = new System.Drawing.Size(242, 340);
-            this.checkedListBox2.TabIndex = 1;
+            this.selectDevice.Location = new System.Drawing.Point(247, 80);
+            this.selectDevice.Name = "selectDevice";
+            this.selectDevice.Size = new System.Drawing.Size(100, 56);
+            this.selectDevice.TabIndex = 3;
+            this.selectDevice.Text = "selectDevice";
+            this.selectDevice.UseVisualStyleBackColor = true;
             // 
             // searchDevice
             // 
-            this.searchDevice.Location = new System.Drawing.Point(6, 364);
+            this.searchDevice.Location = new System.Drawing.Point(247, 6);
             this.searchDevice.Name = "searchDevice";
             this.searchDevice.Size = new System.Drawing.Size(100, 56);
             this.searchDevice.TabIndex = 2;
             this.searchDevice.Text = "searchDevice";
             this.searchDevice.UseVisualStyleBackColor = true;
             // 
-            // selectDevice
+            // checkedListBox2
             // 
-            this.selectDevice.Location = new System.Drawing.Point(142, 364);
-            this.selectDevice.Name = "selectDevice";
-            this.selectDevice.Size = new System.Drawing.Size(100, 56);
-            this.selectDevice.TabIndex = 3;
-            this.selectDevice.Text = "selectDevice";
-            this.selectDevice.UseVisualStyleBackColor = true;
+            this.checkedListBox2.Dock = System.Windows.Forms.DockStyle.Right;
+            this.checkedListBox2.FormattingEnabled = true;
+            this.checkedListBox2.Location = new System.Drawing.Point(417, 3);
+            this.checkedListBox2.Name = "checkedListBox2";
+            this.checkedListBox2.Size = new System.Drawing.Size(242, 504);
+            this.checkedListBox2.TabIndex = 1;
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(3, 3);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(238, 504);
+            this.checkedListBox1.TabIndex = 0;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(662, 510);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "发送命令";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(72, 20);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 21);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "服务器IP:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(137, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "安装服务后即开始运行！";
             // 
             // Form1
             // 
@@ -113,9 +194,13 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(670, 536);
             this.Controls.Add(this.tabControl1);
+            this.MaximizeBox = false;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "门禁工具软件";
             this.tabControl1.ResumeLayout(false);
+            this.tabPage3.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.tabPage1.ResumeLayout(false);
             this.ResumeLayout(false);
 
@@ -130,6 +215,13 @@
         private System.Windows.Forms.Button searchDevice;
         private System.Windows.Forms.CheckedListBox checkedListBox2;
         private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button installService;
+        private System.Windows.Forms.Button checkService;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
