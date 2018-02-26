7a282a47e0b4040b12ec2ee2739e1002b3bcbd39
wei li <liweiliwei0103@outlook.com>
2018-02-26 10:04:42 +0800

    不用dll,否则log4net用不了

--- MenJinWinform/Form1.Designer.cs
+++ MenJinWinform/Form1.Designer.cs
@@ -28,12 +28,108 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.checkedListBox2 = new System.Windows.Forms.CheckedListBox();
+            this.searchDevice = new System.Windows.Forms.Button();
+            this.selectDevice = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(13, 13);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(645, 511);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.selectDevice);
+            this.tabPage1.Controls.Add(this.searchDevice);
+            this.tabPage1.Controls.Add(this.checkedListBox2);
+            this.tabPage1.Controls.Add(this.checkedListBox1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(637, 485);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "搜索设备";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(637, 485);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "发送命令";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(4, 4);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(238, 340);
+            this.checkedListBox1.TabIndex = 0;
+            // 
+            // checkedListBox2
+            // 
+            this.checkedListBox2.FormattingEnabled = true;
+            this.checkedListBox2.Location = new System.Drawing.Point(392, 3);
+            this.checkedListBox2.Name = "checkedListBox2";
+            this.checkedListBox2.Size = new System.Drawing.Size(242, 340);
+            this.checkedListBox2.TabIndex = 1;
+            // 
+            // searchDevice
+            // 
+            this.searchDevice.Location = new System.Drawing.Point(6, 364);
+            this.searchDevice.Name = "searchDevice";
+            this.searchDevice.Size = new System.Drawing.Size(100, 56);
+            this.searchDevice.TabIndex = 2;
+            this.searchDevice.Text = "searchDevice";
+            this.searchDevice.UseVisualStyleBackColor = true;
+            // 
+            // selectDevice
+            // 
+            this.selectDevice.Location = new System.Drawing.Point(142, 364);
+            this.selectDevice.Name = "selectDevice";
+            this.selectDevice.Size = new System.Drawing.Size(100, 56);
+            this.selectDevice.TabIndex = 3;
+            this.selectDevice.Text = "selectDevice";
+            this.selectDevice.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(670, 536);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button selectDevice;
+        private System.Windows.Forms.Button searchDevice;
+        private System.Windows.Forms.CheckedListBox checkedListBox2;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
     }
 }
 
